d2e1dbb746b58c0b27de1a1529403841a0b7d7c5
guillermochussir <guillermochussir@gmail.com>
2021-02-09 21:42:11 -0300

    Compare between cameraCount

--- src/test/java/helpers/CountPhotosByCamera.java
+++ src/test/java/helpers/CountPhotosByCamera.java
@@ -1,5 +1,7 @@
 package helpers;
 import java.util.*;
+
+import com.sun.org.apache.bcel.internal.generic.RETURN;
 import net.minidev.json.JSONArray;
 import com.fasterxml.jackson.databind.*;
 
@@ -17,6 +19,7 @@ public class CountPhotosByCamera {
 		}
 
 		Map<String, Integer> frequencyMap = new HashMap<>();
+		System.out.println("Calculating frequencies...");
 		for (String s: list) {
 			Integer count = frequencyMap.get(s);
 			if (count == null)
@@ -26,11 +29,32 @@ public class CountPhotosByCamera {
             System.out.println(s);
 		}
 
-        System.out.println("Calculating frequencies...");
-
 		for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()) {
 			System.out.println(entry.getKey() + ": " + entry.getValue());
 		}
 
+		String currentCameraName = "";
+		int currentCameraCount = 0;
+		StringBuilder ErrorsFound = new StringBuilder();
+		for (Map.Entry<String, Integer> i : frequencyMap.entrySet()) {
+			System.out.println("Comparing camera:");
+			System.out.println(i.getKey() + ": " + i.getValue());
+
+			currentCameraName = i.getKey();
+			currentCameraCount = i.getValue();
+
+			for (Map.Entry<String, Integer> j : frequencyMap.entrySet()) {
+				System.out.println("to camera:");
+				System.out.println(j.getKey() + ": " + j.getValue());
+				if (currentCameraCount / j.getValue() > 10) {
+					System.out.println(currentCameraName + " Camera Count is 10 times greater than " + j.getKey());
+					ErrorsFound.append(currentCameraName).append(" Camera Count is 10 times greater than ").append(j.getKey()).append(". ");
+					}
+				}
+			}
+
+		System.out.println(ErrorsFound);
+
 		}
-	}
+
+}
